8ca5562e5afe2e282fc2284c33de09c7a25d9e5e
schelip <fcominscheffel@gmail.com>
2024-08-03 16:33:20 -0300

    feat: added tortoise and db configuration

--- project/app/config.py
+++ project/app/config.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 
@@ -8,6 +9,7 @@ log = logging.getLogger('univorn')
 class Settings(BaseSettings):
   environment: str = os.getenv('ENVIRONMENT', 'dev')
   testing: bool = os.getenv('TESTING', 0)
+  database_url: AnyUrl = os.environ.get('DATABASE_URL')
 
 @lru_cache
 def get_settings() -> BaseSettings:
